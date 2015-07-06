ea4a23f0c3e81250e544f2011f1a8a9af3d3cf6c
m.roppy <roppy@hotmail.co.jp>
2015-07-06 17:24:10 +0900

    チョウゾーン追加
    optimization has not been performed.

---
+++ Assets/Script/move.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+	public Transform PointB;
+	private int _direction = 1;
+	private float _pointA;
+	
+	// Use this for initialization
+	IEnumerator Start ()
+	{
+		Debug.Log(this.gameObject.name + "::Start()");
+		_pointA = transform.position.x;
+		while (true)
+		{
+			if (transform.position.x < _pointA)
+			{
+				_direction = 1;
+			}
+			if (transform.position.x > PointB.position.x)
+			{
+				_direction = -1;
+			}
+			transform.Translate(_direction * 2 * Time.deltaTime,0,0);
+			yield return 0;
+		}
+	}
+	// Use this for initialization
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
